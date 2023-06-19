281c87080f35c0808dce2c49e3d2fc0b9e377878
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-06-19 13:24:12 +0530

    mongo url connected

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,15 @@ app.use(cors())
 app.use(express.json())
 app.use("/user",UserRoutes)
 app.use("/admin",adminRoutes)
-
 //to run server check u r in backend folder ->pizzaApp\backend> then type "npm start"
+const MONGO_URL = "mongodb+srv://asamarun2003:pizza1234@cluster0.iz4axej.mongodb.net/?retryWrites=true&w=majority"
+const PORT = 5000;
+mongoose.connect(MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(PORT,()=>console.log(`Mongodb connected port:${PORT}`))
 
-const port=3001
-app.listen(port,()=>console.log(`listening on ${port}`))
+}).catch((err) => {
+    console.log(`${err} didnot connect`)
+})
--- frontend/pizzaApp/components/Login/LoginPage.jsx
+++ frontend/pizzaApp/components/Login/LoginPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import "./loginpage.css"
-function LoginPage() {
-  return (
-    <div>
-      <h1>Login page it should render first</h1>
-    </div>
-  )
-}
+import React from 'react';
+import './loginPage.css';
 
-export default LoginPage
+const LoginPage = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Handle login logic
+  };
+
+  return (
+    <div className="login-page">
+      <div className="container">
+        <h2>Welcome Back!</h2>
+        <form onSubmit={handleLogin}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+          <p className="signup-link">Don't have an account? <a href="/register">Sign up</a></p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
