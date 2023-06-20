d3690e6f5329f5e1c9a8b88bb45e743089ce1092
Avisnura <99018821+Arunsiva003@users.noreply.github.com>
2023-06-20 08:22:49 +0530

    Update LoginPage.jsx

--- frontend/pizzaApp/components/Login/LoginPage.jsx
+++ frontend/pizzaApp/components/Login/LoginPage.jsx
@@ -22,9 +22,7 @@ const LoginPage = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className='entry-contain'>
-=======
     <div className='body-loginpage'>
 >>>>>>> 39d794cea82131de772a2ceb7f58dfe187caf0d3
     <div className='login_container'>
