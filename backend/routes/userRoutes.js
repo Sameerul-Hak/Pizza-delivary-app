e198bf3e610741a37d9efad529b14495afdd8ad0
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-06-20 14:29:39 +0530

    pizzalist and singlepizzadetails-backedn not yet completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,31 @@
-const express=require("express")
-const router=express.Router()
+
 const userController=require("./../controller/usersController")
+const { detailpizzadmin} = require("../controller/adminController")
 
 
+const express = require('express');
+const router = express.Router();
 
-module.exports=router
+// Assuming you have a pizza model or a MongoDB collection called "Pizza"
+const Pizza = require('../model/pizzaSchema');
+
+// Single Pizza Route
+router.get('/singlepizza/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      // Find the pizza by ID
+      const pizza = await Pizza.findById(id);
+    
+      if (!pizza) {
+        return res.status(404).json({ error: 'Pizza not found' });
+      }
+    
+      res.json(pizza);
+    } catch (error) {
+      console.error("error");
+      res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
