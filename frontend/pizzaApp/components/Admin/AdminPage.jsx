27e085088cee2eb878c15ac7035df9c7032daf93
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-06-20 16:13:40 +0530

    styled admin page

--- frontend/pizzaApp/components/Admin/AdminPage.jsx
+++ frontend/pizzaApp/components/Admin/AdminPage.jsx
@@ -3,7 +3,8 @@ import "./adminpage.css"
 import axios from 'axios'
 import DetailPizza from '../DetailPizza/DetailPizza';
 import { Link } from 'react-router-dom';
-
+import pizza from './../../assets/images/icons8-pizza-50.png'
+import price from './../../assets/images/icons8-price-50.png'
 function AdminPage() {
   const [userdetails, setUserdetails] = useState([]);
 
@@ -15,16 +16,28 @@ function AdminPage() {
       },[])
       console.log(userdetails)
       return (
-        <div>
-          <Link to="/createpizzaadmin">create pizza</Link>
+        <div className='body-admin'>
+          <nav className='nav-admin'>
+            <Link className='spl' to="/admin">Admin</Link>
+          <Link className='nav-t' to="/myorders">my orders</Link>
+          <Link className='nav-t' to="/createpizzaadmin">create pizza</Link>
+          </nav>
+          <div className='admin-contianer'>
            {userdetails.map(user => (
-        <a key={user._id} href={`/detailpizza/${user._id}`}>
-          <div>
-            <h3>{user.name}</h3>
-            <img width="100px" height="200px" src={`http://localhost:3001/images/` + user.image} alt='images' />
+             <div key={user._id} className='pizza-block'>
+            <img className='img-tag' width="100px" height="200px" src={`http://localhost:3001/images/` + user.image} alt='images' />
+            <h1 className='name-pizza'  style={{color:"red"}}>{user.name}</h1>
+            <h5 style={{marginLeft:"auto",paddingRight:"20px",fontSize:"20px"}}>Type:{user.typeofpizza}</h5>
+            <hr className='hrtag' />
+            <div className='pqtype'>
+            <h3 className='pqh3'><img width={"40px"} style={{position:"relative",left:"15%"}}  src={price}/> <br/>price: <span style={{color:"red",fontSize:"20px"}}>${user.price}</span></h3>
+            <h3 className='pqh3'><img width={"40px"} style={{position:"relative",left:"15%"}} src={pizza}/> <br/>quantity:<span style={{color:"red",fontSize:"25px"}}>{user.quantity}</span></h3>
+            <h3 className='pqh3'>tags: #{user.tags}</h3>
+            </div>
+            <h3 className='des'>{user.description}</h3>
+            <a key={user._id} href={`/detailpizza/${user._id}`} className='view-details'><h2>veiw details</h2></a>            
           </div>
-        </a>
-      ))}
+      ))}</div>
         </div>
       );
 }
