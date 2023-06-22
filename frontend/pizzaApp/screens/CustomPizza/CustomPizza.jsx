3c5b415f5c25cdf31540ee1090b78714701c0a16
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-06-22 12:47:20 +0530

    custom pizza page layout and cat page quantity and total amount display

--- frontend/pizzaApp/screens/CustomPizza/CustomPizza.jsx
+++ frontend/pizzaApp/screens/CustomPizza/CustomPizza.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
-import "./custompizza.css"
-function CustomPizza() {
+import React, { useState } from 'react';
+
+const CustomPizzaPage = () => {
+  const [selectedBase, setSelectedBase] = useState('');
+  const [selectedSauce, setSelectedSauce] = useState('');
+  const [selectedCheese, setSelectedCheese] = useState('');
+  const [selectedVeggies, setSelectedVeggies] = useState([]);
+
+  const baseOptions = ['Thin Crust', 'Thick Crust', 'Whole Wheat', 'Gluten-Free', 'Cauliflower Crust'];
+  const sauceOptions = ['Marinara', 'Alfredo', 'Pesto', 'BBQ', 'Garlic Butter'];
+  const cheeseOptions = ['Mozzarella', 'Cheddar', 'Parmesan', 'Gouda', 'Feta'];
+  const veggieOptions = ['Tomatoes', 'Onions', 'Bell Peppers', 'Mushrooms', 'Olives', 'Spinach', 'Pineapple'];
+
+  const handleBaseSelection = (base) => {
+    setSelectedBase(base);
+  };
+
+  const handleSauceSelection = (sauce) => {
+    setSelectedSauce(sauce);
+  };
+
+  const handleCheeseSelection = (cheese) => {
+    setSelectedCheese(cheese);
+  };
+
+  const handleVeggieSelection = (veggie) => {
+    if (selectedVeggies.includes(veggie)) {
+      setSelectedVeggies(selectedVeggies.filter((item) => item !== veggie));
+    } else {
+      setSelectedVeggies([...selectedVeggies, veggie]);
+    }
+  };
+
+  const renderOptions = (options, selectedOption, handleSelection) => {
+    return (
+      <ul>
+        {options.map((option) => (
+          <li key={option}>
+            <label>
+              <input
+                type="checkbox"
+                checked={selectedOption === option}
+                onChange={() => handleSelection(option)}
+              />
+              {option}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div>
-      <h1>CUSTOM PIZZA PAGE</h1>
-    </div>
-  )
-}
+      <h2>Customize Your Pizza</h2>
 
-export default CustomPizza
+      <div>
+        <h3>Select Pizza Base:</h3>
+        {renderOptions(baseOptions, selectedBase, handleBaseSelection)}
+      </div>
+
+      <div>
+        <h3>Select Sauce:</h3>
+        {renderOptions(sauceOptions, selectedSauce, handleSauceSelection)}
+      </div>
+
+      <div>
+        <h3>Select Cheese:</h3>
+        {renderOptions(cheeseOptions, selectedCheese, handleCheeseSelection)}
+      </div>
+
+      <div>
+        <h3>Select Veggies:</h3>
+        {veggieOptions.map((veggie) => (
+          <label key={veggie}>
+            <input
+              type="checkbox"
+              checked={selectedVeggies.includes(veggie)}
+              onChange={() => handleVeggieSelection(veggie)}
+            />
+            {veggie}
+          </label>
+        ))}
+      </div>
+
+      <h3>Selected Pizza Configuration:</h3>
+      <p>Base: {selectedBase}</p>
+      <p>Sauce: {selectedSauce}</p>
+      <p>Cheese: {selectedCheese}</p>
+      <p>Veggies: {selectedVeggies.join(', ')}</p>
+    </div>
+  );
+};
+
+export default CustomPizzaPage;
