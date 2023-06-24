45178a271ad345d151a665adf5045bcfce914b62
SuganthAmar <asuganth2003@gmail.com>
2023-06-24 18:13:44 +0530

    navdar designed

--- frontend/pizzaApp/components/Footer.jsx
+++ frontend/pizzaApp/components/Footer.jsx
@@ -1,43 +1,40 @@
 import React from 'react';
+import './footer.css'
 
 function Footer() {
-  const containerStyle = {
-    backgroundColor: '#111',
-    padding: '2rem',
-    textAlign: 'center',
-    borderTop: '1px solid #333',
-    color: '#fff',
-    fontFamily: 'Arial, sans-serif',
-  };
-
-  const textStyle = {
-    fontSize: '0.875rem',
-    marginBottom: '1rem',
-  };
-
-  const linkStyle = {
-    color: '#fff',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-  };
-
-  const logoStyle = {
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    marginBottom: '1rem',
-  };
-
   return (
-    <footer style={containerStyle}>
-      <h2 style={logoStyle}>Pizza Delight</h2>
-      <div style={textStyle}>
-        &copy; {new Date().getFullYear()} Pizza Delight. All rights reserved.
-      </div>
-      <div style={textStyle}>
-        Designed and developed by <a href="https://www.example.com" style={linkStyle}>Your Name</a>
-      </div>
+    <section className="footerbody">
+    <footer className="footer">
+      <div className="footerbox">
+        <h2>Links</h2>
+        <ul class="footerlinks">
+          <li><a href="/">Home</a></li>
+          <li><a href="#aboutus">About Us</a></li>
+          <li><a href="#contactus">Contact Us</a></li>
+          </ul>
+          </div>
+        <div className="footerbox">
+          <h2>News Letter</h2>
+          <form action="" className='footerregistration'>
+            <input type="email" name='email' id='email' placeholder='Enter your email ID'/>
+            <button id='footerbutton' type='submit'>Subscibe Now</button>
+          </form>
+        </div>
+        <div className="footerbox">
+          <h2>Contact</h2>
+          <p className="footeraddress">123, xyz Road, ABC 20 <br />
+          Chennai, Tamil Nadu, India</p>
+          <ul className="footersocial">
+            <li><i className="fa-brands fa-facebook"></i></li>
+            <li><i className="fa-brands fa-instagram"></i></li>
+            <li><i className="fa-brands fa-twitter"></i></li>
+          </ul>
+          </div>
     </footer>
-  );
+    </section>
+);
 }
+
+    
 
 export default Footer;
